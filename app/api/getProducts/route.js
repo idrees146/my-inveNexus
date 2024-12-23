0c6f7257509ed39de4a79fd2b1e7a173f40041f8
Muhammad Idrees <muhammadidr1122@gmail.com>
2024-12-23 07:03:26 -0800

    again

--- app/api/getProducts/route.js
+++ app/api/getProducts/route.js
@@ -1,10 +1,13 @@
-import dbConnect from "/lib/dbConnect";
-import Product from "/models/Product";
+import dbConnect from "../../../lib/dbConnect";
+import Product from "../../../models/Product";
 
-export async function GET(req) {
+export async function GET() {
+
+  await dbConnect();
+
   try {
     // Connect to the database
-    await dbConnect();
+    
 
     // Fetch all products
     const products = await Product.find({});
